1734fc8cf2d91cc3c61f24c51032df027ec5cb45
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-04 14:06:14 +0100

    working list, started css

--- models/order_list.py
+++ models/order_list.py
@@ -1,7 +1,7 @@
 from models.order import *
 
-order1 = Order("Jeremy", "04/08/2021", 3, "Disposable")
-order2 = Order("Dimitar", "02/08/2021", 5, "Buff")
-order3 = Order("Maggie", "01/08/2021", 4, "Reusable")
+order1 = Order("Jeremy", "04/08/2021", 3, "disposable")
+order2 = Order("Dimitar", "02/08/2021", 5, "buff")
+order3 = Order("Maggie", "01/08/2021", 4, "reusable")
 
 orders = [order1, order2, order3]

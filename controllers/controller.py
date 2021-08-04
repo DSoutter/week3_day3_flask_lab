f6aa56809ba56173a81cf84258511b08833efde1
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-04 15:16:22 +0100

    lab completed with extension and extra extension

--- controllers/controller.py
+++ controllers/controller.py
@@ -9,3 +9,7 @@ from models.order_list import orders
 @app.route('/orders')
 def index_orders():
     return render_template('index.html', title="CodeClan", orders=orders)
+
+@app.route('/orders/<index>')
+def individual_index(index):
+    return render_template('order.html', title="Customer's Order", order=orders[int(index)])

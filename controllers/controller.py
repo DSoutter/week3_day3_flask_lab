04c9bb3159961da4b1eafcbc23a6f2be990efe21
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-04 13:48:49 +0100

    added order class

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,11 @@
 from app import app
 from flask import render_template
+from models.order_list import orders
 
+# @app.route('/')
+# def index():
+#     return "placeholder"
+
+@app.route('/orders')
+def index_orders():
+    return render_template('index.html', title="Mask Shop", orders=orders)
---
+++ models/order_list.py
@@ -0,0 +1,7 @@
+from models.order import *
+
+order1 = Order("Jeremy", "04/08/2021", 3, "Disposable")
+order2 = Order("Dimitar", "02/08/2021", 5, "Buff")
+order3 = Order("Maggie", "01/08/2021", 4, "Reusable")
+
+orders = [order1, order2, order3]
